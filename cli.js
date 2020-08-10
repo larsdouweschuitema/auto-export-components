ebc0a1b690c614dc759a4e04550e9d669f483396
larsschuitema <larsschuitema@funda.nl>
2020-08-10 23:19:08 +0200

    improve setup cli

--- cli.js
+++ cli.js
@@ -1,65 +1,92 @@
 #!/usr/bin/env node
 const { join, resolve } = require('path');
 const { readdirSync, existsSync, unlink, writeFile, appendFile } = require('fs');
-const indexFile = 'index.js';
-const directoryPath = join(resolve(), 'components');
+const { camelCase } = require('lodash');
+
+const dir = join(resolve(), 'src/components');
+const indexPath = 'src/index.js';
+const extension = '.vue';
 let data = '';
-let components;
+let components = [];
+
+function filterByVueExtension(file) {
+    return file.indexOf(extension) !== -1;
+}
+
+function getFilesFromDir() {
+    return readdirSync(dir).filter(filterByVueExtension);
+}
+
+function removeExtensionFromFiles(items) {
+    let componentsArr = [];
+    items.forEach(item => {
+        let tempItem = {};
+        tempItem['name'] = createComponentName(item);
+        tempItem['file'] = item;
+        componentsArr.push(tempItem);
+    });
+    return componentsArr;
+}
 
 function removeExtension(file) {
     return file.split('.').slice(0, -1).join('.');
 }
 
-function getFilesByName(files) {
-    let filesArray = [];
-    files.forEach(file => {
-        const fileName = removeExtension(file);
-        filesArray.push(fileName);
-    });
+function createComponentName(component) {
+    let tempComponentName;
+    tempComponentName = removeExtension(component);
+    tempComponentName = camelCase(tempComponentName);
+    tempComponentName = capitalizeFirstLetter(tempComponentName);
+    return tempComponentName;
+}
 
-    return filesArray;
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function getComponents() {
-    const files = readdirSync(directoryPath);
-    components = getFilesByName(files);
+    const componentsArr = getFilesFromDir();
+    components = removeExtensionFromFiles(componentsArr);
 }
 
 function createDataString() {
-  components.forEach(component => {
-    data += `
-  import ${component} from './components/${component}.mjs'; `;
-  });
+    if(components.length) {
+        components.forEach(component => {
+        data += `
+import ${component.name} from './components/${component.file}'; `;
+        });
 
-  data += `
-  export { ${components} }`;
+        data += `
+
+export { ${components.map(component => component.name)} };`;
+    }
 }
 
-function logErr(err) {
-    if(err) throw err;
+function logError(err) {
+    if (err) throw err;
 }
 
-function fileExists() {
-  return existsSync(indexFile) ? true : false;
+function deleteIndexFile() {
+    unlink(indexPath, (err) => logError(err));
 }
 
-function deleteFile() {
-  unlink(indexFile, (err) => logErr(err));
+function createIndexFile() {
+    writeFile(indexPath, '', (err) => logError(err));
 }
 
-function createFile() {
-  writeFile(indexFile, '', (err) => logErr(err));
-}
-
-function addContentToFile() {
-  appendFile(indexFile, data, (err) => logErr(err));
+function addDataToFile() {
+    appendFile(indexPath, data, (err) => logError(err));
 }
 
 function exportComponents() {
-  fileExists() ? deleteFile() : createFile();
-  addContentToFile();
+    existsSync(indexPath) ? deleteIndexFile() : createIndexFile();
+    addDataToFile();
 }
 
-getComponents();
-createDataString();
-exportComponents();
+function autoExportComponents() {
+    getComponents();
+    createDataString();
+    exportComponents();
+}
+
+autoExportComponents();
